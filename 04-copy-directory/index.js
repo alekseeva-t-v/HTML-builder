50aae35db0c7e9c3822886c73467573b76bc66ad
Tatyana Alekseeva <alekseeva.tatiana.vi@gmail.com>
2023-05-04 06:22:50 +0300

    feat: fourth task completed

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,27 @@
+const fs = require('fs');
+const path = require('path');
 
+(function copyDir() {
+  fs.mkdir(
+    path.join(__dirname, 'files-copy'),
+    { recursive: true, force: true },
+    (err) => {
+      if (err) throw err;
+    }
+  );
+  fs.promises.readdir(path.join(__dirname, 'files-copy')).then((elements) => {
+    elements.forEach((element) => {
+      fs.promises.unlink(path.join(__dirname, 'files-copy', element), (err) => {
+        if (err) throw err;
+      });
+    });
+  });
+  fs.promises.readdir(path.join(__dirname, 'files')).then((elements) => {
+    elements.forEach((element) => {
+      fs.promises.copyFile(
+        path.join(__dirname, 'files', element),
+        path.join(__dirname, 'files-copy', element)
+      );
+    });
+  });
+})();
